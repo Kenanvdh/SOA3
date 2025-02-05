650bb18092579e90eb4495a37c2e4c7362da5ff6
Kenanvdh <kenanvdh@ziggo.nl>
2025-02-05 11:47:32 +0100

    assembly info

--- BioscoopConsole/BioscoopConsole/obj/Debug/net8.0/BioscoopConsole.AssemblyInfo.cs
+++ BioscoopConsole/BioscoopConsole/obj/Debug/net8.0/BioscoopConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioscoopConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf8aa54558afc05b5d6ae0bbb2506ffca57edc7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f231f20af642d13b63ca63b7d7cde0f48d775941")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioscoopConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioscoopConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
